0728dc6cee7ac3c8f6a9fa6e7034c64135f12995
Trong Phuoc <phuoctt@rabiloo.com>
2025-06-13 16:29:31 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("audio-app-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.json",
+        "./icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
